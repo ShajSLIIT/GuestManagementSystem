f9e6e454a2304d54614b51e7b4e61464f4c7a4f1
ShajSLIIT <it24610789@my.sliit.lk>
2025-08-21 14:47:09 +0530

    Added the Starter Project

---
+++ src/main/java/com/ghm/guesthousemanagement/GuestHouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ghm.guesthousemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestHouseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestHouseManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghm/guesthousemanagement/GuestHouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghm.guesthousemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestHouseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
